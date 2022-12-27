51ec40f49d3d29bfab7429540c8ed488cd585866
San4ezzz <rahlinskiysasha@gmail.com>
2022-12-27 22:25:25 +0100

    Creating a custom dialog

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.weatherapp
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
@@ -32,6 +33,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private var mProgressDialog: Dialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -112,12 +115,15 @@ class MainActivity : AppCompatActivity() {
                 latitude, longitude,Constants.METRIC_UNIT, Constants.API_ID
             )
 
+            showCustomProgressDialog()
+
             listCall.enqueue(object : Callback<WeatherResponse> {
                 override fun onResponse(
                     call: Call<WeatherResponse>,
                     response: Response<WeatherResponse>
                 ) {
                     if(response.isSuccessful) {
+                        hideProgressDialog()
                         val weatherList: WeatherResponse = response.body()!!
                         Log.i("Response Result","$weatherList")
                     } else {
@@ -136,6 +142,7 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
                     Log.e("Error!", t.message.toString())
+                    hideProgressDialog()
                 }
 
             })
@@ -172,5 +179,19 @@ class MainActivity : AppCompatActivity() {
             getSystemService(Context.LOCATION_SERVICE) as LocationManager
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
                 || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun showCustomProgressDialog() {
+        mProgressDialog = Dialog(this)
+
+        mProgressDialog!!.setContentView(R.layout.dialog_custom_progress)
+
+        mProgressDialog!!.show()
+    }
+
+    private fun hideProgressDialog() {
+        if(mProgressDialog != null) {
+            mProgressDialog!!.dismiss()
+        }
     }
 }
