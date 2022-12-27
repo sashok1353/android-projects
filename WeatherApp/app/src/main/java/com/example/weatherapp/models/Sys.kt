f89af15ba416373bbd889e1d64327ef5d434159b
San4ezzz <rahlinskiysasha@gmail.com>
2022-12-27 21:38:05 +0100

    Preparing the models based on the API

---
+++ WeatherApp/app/src/main/java/com/example/weatherapp/models/Sys.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp.models
+
+import java.io.Serializable
+
+data class Sys(
+    val type: Int,
+    val message: Double,
+    val id: Int,
+    val country: String,
+    val sunrise: Int,
+    val sunset: Int,
+): Serializable
