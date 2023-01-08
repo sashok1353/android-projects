2373c6301a1a164786922f29c7f5d5ac640c89b3
San4ezzz <rahlinskiysasha@gmail.com>
2023-01-08 16:16:04 +0100

    Setting up sign in activity

--- Projemanag/app/src/main/java/com/example/projemanag/IntroActivity.kt
+++ Projemanag/app/src/main/java/com/example/projemanag/IntroActivity.kt
@@ -23,5 +23,8 @@ class IntroActivity : AppCompatActivity() {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
 
+        binding?.btnSignInIntro?.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
     }
 }
---
+++ Projemanag/app/src/main/java/com/example/projemanag/SignInActivity.kt
@@ -0,0 +1,38 @@
+package com.example.projemanag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.example.projemanag.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+
+    var binding: ActivitySignInBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding?.toolbarSignInActivity)
+
+        val actionBar = supportActionBar
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+
+        binding?.toolbarSignInActivity?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
